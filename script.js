86d71b8fcb2794b1f86be785cbc3f35b1925412f
Slyfer0 <116066931+Slyfer0@users.noreply.github.com>
2024-02-08 21:12:25 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
-document.addEventListener("DOMContentLoaded", function (){
-
-    const bt = document.getElementById("bt");
+document.addEventListener("DOMContentLoaded", function () {
+    const txtBt = document.getElementById("txtBt");
     const price = document.getElementById("price");
+    const txtPesos = document.getElementById("txtPesos");
+    const rate = 2.4;
 
-    bt.addEventListener("input", function(){
-        const amount = bt.value;
+    function updateValues(input, output) {
+        const amount = input.value.trim();
         const isInteger = /^\d+$/.test(amount);
-        const checkInput = bt.value.trim();
 
-        if(isInteger && !isNaN(amount)){
-            const gprice = Math.ceil(amount/0.7);
-            const test = gprice.toLocaleString();
-            price.innerHTML = "R$ " + test;
-        } else if(checkInput === ""){
+        if (isInteger && !isNaN(amount)) {
+            let convertedValue;
+            if (input === txtBt) {
+                convertedValue = Math.ceil(amount / 0.7);
+                txtPesos.value = Math.ceil(amount / rate).toLocaleString();
+            } else if (input === txtPesos) {
+                robux = Math.floor(amount * rate);
+                convertedValue = Math.ceil(robux / 0.7).toLocaleString();
+                txtBt.value = robux;
+            }
+
+            const formattedValue = convertedValue.toLocaleString();
+            price.innerHTML = "R$ " + formattedValue;
+        } else {
             price.innerHTML = "";
-        } else{
-            price.innerHTML = "Invalid Input";
+            input.value = ""; // Clear the input field for invalid input
+            if (input === txtPesos) {
+                txtBt.value = ""; // Clear txtBt if txtPesos is empty
+            } else if (input === txtBt) {
+                txtPesos.value = ""; // Clear txtPesos if txtBt is empty
+            }
         }
+    }
+
+    txtPesos.addEventListener("input", function () {
+        updateValues(txtPesos, txtBt);
+    });
+
+    txtBt.addEventListener("input", function () {
+        updateValues(txtBt, txtPesos);
     });
 });
